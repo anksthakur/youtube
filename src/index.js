a64e0dca80bc804a36401844b7bc875b41786186
anup thakur <anksthakur7@gmail.com>
2025-08-19 23:21:25 +0530

    updating app.js also index.js file in root .then.catch app.listen

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-
 import dotenv from "dotenv";
-import connectDB from "../src/db/index.js"
-
+import connectDB from "../src/db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+const PORT = process.env.PORT || 3002
 
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo Db connection failed", error);
+  });
 
 // import mongoose from "mongoose"
 // import { DB_NAME } from "./constants"
