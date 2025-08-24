cce66d9e19338eacac1289eb63847c6cc16a1919
anup thakur <anksthakur7@gmail.com>
2025-08-24 18:57:22 +0530

    making middleware for logout and logout function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -108,7 +108,19 @@ return res.status(200).cookie("accessToken",accessToken,options)
 })
 
 const logoutUser = asyncHandler(async (req,res)=>{
-
+await User.findByIdAndUpdate(req.user._id ,{
+  $set:{
+    refreshToken:undefined
+  }
+ })
+ const options ={
+  httpOnly:true,
+  secure:true
+}
+return res.status(200)
+.clearCookie("accessToken",options)
+.clearCookie("refreshToken",options)
+.json(new ApiResponse(200,{},"user logged out"))
 })
 
 export { registerUser,loginUser,logoutUser };
